f01f3ce41c5d62bada442e557ae5e3ba69232651
davehh1211 <davehh1211@gmail.com>
2020-07-01 17:11:37 +0000

    add place tests and fix comparison

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -17,7 +17,7 @@ class TestBaseModel(unittest.TestCase):
     def test_base_pep8_conformance_base_model(self):
         """Test that we conform to PEP8."""
         pep8style = pep8.StyleGuide(quiet=True)
-        result = pep8style.check_files(['models/base_model.py'])
+        result = pep8style.check_files(['./models/base_model.py'])
         self.assertEqual(result.total_errors, 0)
 
     def test_instances(self):
